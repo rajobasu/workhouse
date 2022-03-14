deed5aea9d4474a16c01393036b76715f2dd9de2
SajalVaishnav <76965917+SajalVaishnav@users.noreply.github.com>
2022-03-14 20:10:34 +0800

    volunteer page

--- src/pages/Volunteer.tsx
+++ src/pages/Volunteer.tsx
@@ -1,10 +1,126 @@
-import React from "react";
-import { Text, WhiteBaseContainer } from "../css/css";
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
-export const Volunteer: React.FC = () => {
+interface Column {
+  id: 'name' | 'date' | 'address' | 'shift' | 'calories';
+  label: string;
+  minWidth?: number;
+  align?: 'center';
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'date', label: 'Date', minWidth: 100 },
+  {
+    id: 'address',
+    label: 'Address',
+    minWidth: 170,
+  },
+  {
+    id: 'shift',
+    label: 'Shift',
+    minWidth: 170,
+  },
+  {
+    id: 'calories',
+    label: 'Calories Burnt',
+    minWidth: 170,
+  },
+];
+
+interface Data {
+  name: string;
+  date: string;
+  address: string;
+  shift: string;
+  calories: number;
+}
+
+function createData(
+  name: string,
+  date: string,
+  address: string,
+  shift: string,
+  calories: number
+): Data {
+  return { name, date, address, shift, calories };
+}
+
+const rows = [
+  createData('Hougang', '01/04/2022 - 30/04/2022', '95 Hougang Avenue 4 Hougang Swimming Complex Singapore 538830', 'Multiple Shifts', 450),
+  createData('Sengkang', '10/04/2022 - 15/04/2022', '57 Anchorvale Road #02-07 Sengkang Active Health Lab Singapore 544964', '10am - 6pm', 200)
+];
+
+export const Volunteer = () => {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
-    <WhiteBaseContainer>
-      <Text>This is the volunteer page.</Text>
-    </WhiteBaseContainer>
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: '100%' }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead >
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                  color='black'
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
-};
+}
